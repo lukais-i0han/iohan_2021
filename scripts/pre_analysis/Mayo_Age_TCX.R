29745e4fd5722a974637b557f63595a7eec5d5fb
Lukas Iohan <lukas@neuro.ufrn.br>
2022-01-03 11:23:53 -0300

    Update Mayo_Age_TCX.R

--- scripts/pre_analysis/Mayo_Age_TCX.R
+++ scripts/pre_analysis/Mayo_Age_TCX.R
@@ -4,7 +4,7 @@ library('DESeq2')
 library('stringr')
 library('dplyr')
 
-##Metadado
+## Create the metadata table to data from Human Temporal Cortex of Mayo dataset
 
 mayo_metadado <- read.csv("refs/MayoRNAseq_RNAseq_TCX_covariates.csv",
                              stringsAsFactors = F,header = T)
@@ -27,7 +27,7 @@ mayo_metadado <- mayo_metadado %>%
     AgeAtDeath >= 70 & AgeAtDeath <= 80 ~ 'A',
     AgeAtDeath >= 81 & AgeAtDeath <= 89 ~ 'B',
     AgeAtDeath == 90 ~ 'C',
-    TRUE ~ 'Z'
+    TRUE ~ 'Z' ## exclude patients with age below 70
   )) 
 
 mayo_metadado$'Diag_Age' <- paste(mayo_metadado$condition,mayo_metadado$AgeAtDeath,sep='_')
@@ -42,17 +42,18 @@ write.csv(mayo_metadado,'results/mayo_metadado.csv')
 files = paste0(list.files("kallisto_TCX", full.names=T), "/abundance.tsv")
 files = grep(paste0(paste0("/",mayo_metadado$ID), collapse="|"), files, value=T)
 
-tx2gene <- read.table("refs/tg2.txt", header = T, stringsAsFactors = F)
+tx2gene <- read.table("refs/tg2.txt", header = T, stringsAsFactors = F) ### table with correspondece between transcripts and its respective genes
 
 kallistoQuant <- tximport(files, type = 'kallisto',tx2gene = tx2gene[,c(1,2)])
-## kallistoQuant <- tximport(files, type = 'kallisto',txOut = T)
 colnames(kallistoQuant$abundance) <- mayo_metadado$ID
 colnames(kallistoQuant$counts) <- mayo_metadado$ID
 
-## counts_TCX <- data.frame(kallistoQuant$counts,stringsAsFactors = F)
-#write.csv(counts_TCX,'results/new_results/counts_trancript_TCX.csv')
+### Make table with transcript abundance to later analysis with IsoformSwitch
+kallistoQuant_ISO <- tximport(files, type = 'kallisto',txOut = T)
+counts_TCX <- data.frame(kallistoQuant_ISO$counts,stringsAsFactors = F)
+write.csv(counts_TCX,'results/new_results/counts_trancript_TCX.csv')
 
-### Desesq2 for gene-level
+### Run DESeq2 with Age as the design variable
 
 dds <- DESeqDataSetFromTximport(kallistoQuant,colData = mayo_metadado,design = ~Diag_Age)
 keep <- rowSums(counts(dds)) >=10
