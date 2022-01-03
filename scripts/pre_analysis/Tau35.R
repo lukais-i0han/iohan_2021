aec809a94cbdf5079d3473c03a15abf44b900bab
Lukas Iohan <lukas@neuro.ufrn.br>
2022-01-03 11:46:11 -0300

    Update Tau35.R

--- scripts/pre_analysis/Tau35.R
+++ scripts/pre_analysis/Tau35.R
@@ -4,7 +4,7 @@ library('tximport')
 library('DESeq2')
 library('stringr')
 
-##Metadado
+## Create the metadata table to data TauD35 dataset
 
 Tau_individual <- read.csv('metadata/TauD35_Covariates.csv',header = T,stringsAsFactors = F)
 
@@ -33,13 +33,11 @@ Tau_individual <- Tau_individual %>% mutate(sex = case_when(
 
 write.csv(Tau_individual,'Tau_metadado.csv')
 
-
-
 ### Import Kallisto files
 
 files = paste0(list.files("kallisto", full.names=T), "/abundance.tsv")
 
-tx2gene <- read.table("refs/tg2.txt", header = F, stringsAsFactors = F)
+tx2gene <- read.table("refs/tg2.txt", header = F, stringsAsFactors = F) ### table with correspondece between transcripts and its respective genes
 
 kallistoQuant <- tximport(files, type = 'kallisto',tx2gene = tx2gene[,c(1,3)])
 
@@ -49,17 +47,17 @@ Tau_individual <- Tau_individual[order(Tau_individual$BamFileName),]
 colnames(kallistoQuant[["abundance"]]) <- Tau_individual$id
 colnames(kallistoQuant[["counts"]]) <- Tau_individual$id
 rownames(Tau_individual) <- Tau_individual$id
-### Desesq2 for gene-level
+
+### Make table with transcript abundance to later analysis with IsoformSwitch
+kallistoQuant_ISO <- tximport(files, type = 'kallisto',txOut = T)
+counts_TauD35 <- data.frame(kallistoQuant_ISO$counts,stringsAsFactors = F) 
+write.csv(counts_TauD35,'counts_TauD35.csv')
+
+### Run DESeq2 with Condition as the design variable
 
 dds <- DESeqDataSetFromTximport(kallistoQuant,colData = Tau_individual,design = ~Condition)
 keep <- rowSums(counts(dds)) >=10
 dds <- dds[keep,]
-
-vsd_TAU <- varianceStabilizingTransformation(dds,blind = F, fitType = 'local')
-vsd_TAU <- assay(vsd_TAU)
-saveRDS(vsd_TAU,file='results_deseq/vsd_TAU.rds')
-
-dds <- DESeq(dds,fitType = 'local')
-
+dds <- DESeq(dds)
 saveRDS(dds,file='results/Tau35.rds')
 
